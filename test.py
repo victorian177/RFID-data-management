3d91aa555199082a6aaf2583c140b29fcee247a2
Victor Momodu <victoramomodu@gmail.com>
2021-11-16 12:23:16 +0100

    Completed register, remove, run operations

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-import pandas
+# import pandas
 
-d = {
-    "first": ["Victor", "Jill", "Parker"],
-    "last": ["Gerson", "Patterson", "Bridgerton"],
-    "id": ["A12", "B30", "C49"],
-}
-a = pandas.DataFrame(d)
-a.set_index('id', inplace=True)
-a.drop(index="A12", inplace=True)
+# d = {
+#     "first": ["Victor", "Jill", "Parker"],
+#     "last": ["Gerson", "Patterson", "Bridgerton"],
+#     "id": ["A12", "B30", "C49"],
+# }
+# a = pandas.DataFrame(d)
+# a.set_index('id', inplace=True)
+# a.drop(index="A12", inplace=True)
 
-print(a)
+# # print(a)
+# # s: First Name, Last Name
+# # i: Age
+# # c: Sex|Male|Female
+# # server storage warehouse floor office powerhouse
+
+a = "Hello, my name is Victor Momodu. I am currently a student at Obafemi Awolowo University, Ile-Ife, Nigeria. I am in my second year, studying Electrical and Electronics Engineering. My only source of income is from my parents and occasionally jobs I get. This coupled with the economic hardships in my country has made my income barely enough to handle all my essential expenses thus, paying for this course would be very challenging indeed. The course's cost is very well justified given the immense benefits it offers to everyone who has partaken in it seen by its 4.8-star ratings. Had I the financial ability to afford it, it would be a no-brainer purchase, I am certain I would not regret it the slightest bit. This, unfortunately, is not the case and thus, getting Financial Aid will help me improve both my skill set and resume, both of which I hope to apply to good use. Thanks in advance."
+print(len(a.split()))
--- web_app/main.py
+++ web_app/main.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("C:/Users/Victor Momodu/Documents/Programming/Arduino/Code/RFID-data-management")
 
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, request, jsonify
 import purpose
 import json
 
@@ -51,6 +51,7 @@ def register():
     result = facility_data_findr(name, existing=True)
     instance = purpose_pickr(name, result[name])
     args = []
+    places = instance.accss_plcs if result[name] == "Access" else ""
     try:
         idntfr = request.form["id"]
     except KeyError:
@@ -59,9 +60,11 @@ def register():
         args.append(idntfr)
         for i in instance.data.columns:
             args.append(request.form[i])
-        print(args)
-        instance.register(args=args)
-    return render_template("register.html", name=name, use_case=result[name], dataframe=instance.data)
+        if result[name] == "Access":
+            instance.register(args=args, place_data=request.form["places"])
+        else:
+            instance.register(args=args)
+    return render_template("register.html", name=name, use_case=result[name], dataframe=instance.data, places=places)
 
 @app.route('/remove', methods=["GET", "POST"])
 def remove():
@@ -76,9 +79,25 @@ def remove():
         instance.remove(idntfr)
     return render_template("remove.html", name=name, use_case=result[name], dataframe=instance.data)
 
-@app.route('/run')
+@app.route('/run', methods=["GET", "POST"])
 def run():
-  return render_template("run.html")
+    name = active()
+    result = facility_data_findr(name, existing=True)
+    instance = purpose_pickr(name, result[name])
+    places = instance.accss_plcs if result[name] == "Access" else ""
+    idntfr = {'idntfr': "1249"}
+    try:
+        request.form["id"]
+    except KeyError:
+        pass
+    else:
+        if result[name] == "Access":
+            instance.access_checkr(request.form['id'], request.form["places"])
+        elif result[name] == "Attendance":
+            instance.attendance_logger(request.form["id"])
+        elif result[name] == "Record":
+            instance.record_editor(request.form["id"], request.form["data"])
+    return render_template("run.html", name=name, use_case=result[name], places=places, idntfr=idntfr)
 
 def facility_data_findr(name, use_case="", existing=False):
     if existing:
@@ -111,6 +130,7 @@ def facility_data_findr(name, use_case="", existing=False):
                 facility_data[name] = use_case
                 with open("facility_data.json", 'w') as facility_file:
                     json.dump(facility_data, facility_file)
+                return facility_data
             else:
                 return {name: "Error: instance of this facililty exists in our database. Try using another name."}
 
